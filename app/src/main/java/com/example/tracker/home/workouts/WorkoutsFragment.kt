cf7c48b647292b0bed1ad827e1ddc8dcb056985b
Jean C. T <jeanc.torres@gmail.com>
2019-11-09 17:53:26 +0000

    ...

--- app/src/main/java/com/example/tracker/home/workouts/WorkoutsFragment.kt
+++ app/src/main/java/com/example/tracker/home/workouts/WorkoutsFragment.kt
@@ -4,18 +4,77 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tracker.R
+import com.example.tracker.databinding.WorkoutsFragmentBinding
+import com.example.tracker.home.common.data.entities.XWorkout
+import java.util.logging.Logger
 
 class WorkoutsFragment : Fragment() {
 
     private lateinit var exercisesViewModel: ExercisesViewModel
 
+
     override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        exercisesViewModel = ViewModelProviders.of(this).get(ExercisesViewModel::class.java)
+
+        var binding = DataBindingUtil.inflate<WorkoutsFragmentBinding>(
+            inflater,
+            R.layout.workouts_fragment,
+            container,
+            false
+        )
+            .apply {
+
+                viewModel = exercisesViewModel
+                lifecycleOwner = viewLifecycleOwner
+                callback = object : Callback {
+                    override fun action() {
+                        println("ACTION!")
+
+                    }
+                }
+
+                val adapter = WorkoutsListAdapter(requireContext())
+                recyclerView.adapter = adapter
+
+
+                recyclerView.layoutManager =
+                    LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+
+                this.lifecycleOwner?.let {
+                    exercisesViewModel.workouts.observe(it, Observer { workouts ->
+                        workouts?.let {
+                            println("************************ Fragment XUserAndWorkoutsAndExercises:  ${it.size} UserAndWorkouts")
+                            if (it.size > 0) {
+                                println("${it[0].user} user")
+                                println("${it[0].workoutsAndExercises.size} workoutsAndExercises")
+                                adapter.setWorkoutsAndExercises(it[0].workoutsAndExercises)
+                            }
+                        }
+                    })
+                }
+
+            }
+
+        return binding.root
+    }
+
+    interface Callback {
+        fun action()
+    }
+
+
+    fun onCreateViewOld(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,18 +84,19 @@ class WorkoutsFragment : Fragment() {
         val recyclerView = root.findViewById<RecyclerView>(R.id.recycler_view)
 
 
-        val adapter = WorkoutsListAdapter(requireContext() )
+        val adapter = WorkoutsListAdapter(requireContext())
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
 
         exercisesViewModel = ViewModelProviders.of(this).get(ExercisesViewModel::class.java)
 
         exercisesViewModel.workouts.observe(this, Observer { workouts ->
-            workouts?.let{
+            workouts?.let {
                 println("************************ Fragment XUserAndWorkoutsAndExercises:  ${it.size} UserAndWorkouts")
-                if(it.size > 0){
-                    println("${it[0].user} user" )
-                    println("${it[0].workoutsAndExercises.size} workoutsAndExercises" )
+                if (it.size > 0) {
+                    println("${it[0].user} user")
+                    println("${it[0].workoutsAndExercises.size} workoutsAndExercises")
                     adapter.setWorkoutsAndExercises(it[0].workoutsAndExercises)
                 }
             }
